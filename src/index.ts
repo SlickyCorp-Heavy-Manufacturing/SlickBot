4b51466fce42f9a470a622bbcd1ec337c669aba5
Darrell Faber <dmfaber1@gmail.com>
2020-08-17 13:10:29 -0500

    hurr durr i'm a web developer

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,7 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
+  // Mike's stupid bot replies
   switch(msg.content) {
     case 'ping':
       msg.reply('pong');
@@ -39,5 +40,10 @@ bot.on('message', msg => {
     case '!commands':
       msg.reply('!help for commands');
       break;
+  }
+
+  // Darrell's stupid bot replies
+  if(msg.author == '?Pulak' && msg.channel == 'thing-i-would-buy') {
+    msg.reply('https://i.redd.it/ng2ewzvfado21.jpg')
   }
 });
