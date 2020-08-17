4054633f5904fb758022e34b2a58cb499dcfb6a6
UpsidePotential <mikeafuchs@gmail.com>
2020-08-16 20:05:57 -0500

    use tsc

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+require('dotenv').config();
+import Discord from 'discord.js';
+
+var schedule = require('node-schedule');
+
+import { Weather } from './weather'
+
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+
+bot.login(TOKEN);
+
+
+
+schedule.scheduleJob('* * 8 * *', function() {
+  Weather.currentWeather().then( value => {
+    const channel = bot.channels.find(channel => (channel as Discord.TextChannel).name === 'noaa-information-bureau');
+    (channel as Discord.TextChannel).send(value);
+  })
+});
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+  switch(msg.content) {
+    case 'ping':
+      msg.reply('pong');
+      break;
+    case '!weather':
+        Weather.currentWeather().then( value => {
+          msg.channel.send(value);
+        })
+      break;
+    case '!help':
+      msg.reply('no');
+      break;
+    case '!commands':
+      msg.reply('!help for commands');
+      break;
+  }
+});
---
+++ src/weather.ts
@@ -0,0 +1,12 @@
+
+import got from 'got';
+
+export class Weather {
+    private static readonly MKE_FORECAST: string =  'https://api.weather.gov/zones/land/WIZ066/forecast';
+
+    public static async currentWeather(): Promise<string> {
+        const response = await got(Weather.MKE_FORECAST, {responseType: 'json'})
+        const forcast = (response.body as any).properties.periods[0];
+        return `${forcast.name} ${forcast.detailedForecast}`;
+    }
+}
