a07ae0d209535ffdc63d6cb4cf760d9bc6be1fd9
UpsidePotential <mikeafuchs@gmail.com>
2020-08-18 16:50:03 -0500

    fix some tests

--- integration/slickbot.spec.ts
+++ integration/slickbot.spec.ts
@@ -27,16 +27,20 @@ describe('slickbot', () => {
             commands.forEach((command): void => command.command(msg));
         });
 
-    });
+    }, 10000);
     
     it('ping should post pong', (done) => {
         const testChannel = findChannelByName(_userClient.client, TEST_CHANNEL)
         testChannel.send('ping');
 
-        _lastMessage.pipe(filter(msg => msg.author.username === 'TestSlickBot')).pipe(take(1)).subscribe( msg => {
-            expect(msg.content).toBe('pong');
-            done();
-        });
+        _lastMessage
+            .pipe(filter(msg => msg.author.username === 'TestSlickBot'))
+            .pipe(filter(msg => msg.content === 'pong'))
+            .pipe(take(1))
+            .subscribe( msg => {
+                expect(msg.content).toBe('pong');
+                done();
+            });
     });
 
     it('weather should post the weather', (done) => {
@@ -47,62 +51,81 @@ describe('slickbot', () => {
             expect(msg.content.length > 5)
             done();
         });
-    });
+    }, 10000);
 
     it('troutslap should slap everyone', (done) => {
         const testChannel = findChannelByName(_userClient.client, TEST_CHANNEL)
         testChannel.send('!troutslap @everyone');
 
-        _lastMessage.pipe(filter(msg => msg.author.username === 'TestSlickBot')).pipe(take(1)).subscribe( msg => {
-            expect(msg.content).toContain('slaps')
-            done();
-        });
+        _lastMessage
+            .pipe(filter(msg => msg.author.username === 'TestSlickBot'))
+            .pipe(filter(msg => msg.content.includes('slaps')))
+            .pipe(take(1))
+            .subscribe( msg => {
+                expect(msg.content).toContain('slaps');
+                done();
+            });
     });
 
     it('xkcd should post xkcd', (done) => {
         const testChannel = findChannelByName(_userClient.client, TEST_CHANNEL)
         testChannel.send('!xkcd');
 
-        _lastMessage.pipe(filter(msg => msg.author.username === 'TestSlickBot')).pipe(take(1)).subscribe( msg => {
-            expect(msg.content).toContain('.png')
-            done();
-        });
-    });
+        _lastMessage
+            .pipe(filter(msg => msg.author.username === 'TestSlickBot'))
+            .pipe(filter(msg => msg.content.includes('.png')))
+            .pipe(take(1))
+            .subscribe( msg => {
+                expect(msg.content).toContain('.png')
+                done();
+            });
+    }, 10000);
 
     it('xkcd should post todays xkcd', (done) => {
         const testChannel = findChannelByName(_userClient.client, TEST_CHANNEL)
         testChannel.send('!xkcd today');
 
-        _lastMessage.pipe(filter(msg => msg.author.username === 'TestSlickBot')).pipe(take(1)).subscribe( msg => {
-            expect(msg.content).toContain('.png')
-            done();
-        });
-    });
+        _lastMessage
+            .pipe(filter(msg => msg.author.username === 'TestSlickBot'))
+            .pipe(filter(msg => msg.content.includes('.png')))
+            .pipe(take(1))
+            .subscribe( msg => {
+                expect(msg.content).toContain('.png')
+                done();
+            });
+    }, 10000);
 
     it('help should post help', (done) => {
         const testChannel = findChannelByName(_userClient.client, TEST_CHANNEL)
         testChannel.send('!help');
 
-        _lastMessage.pipe(filter(msg => msg.author.username === 'TestSlickBot')).pipe(take(1)).subscribe( msg => {
-            expect(msg.content).toContain('!help');
-            expect(msg.content).toContain('ping');
-            expect(msg.content).toContain('!weather');
-            expect(msg.content).toContain('!troutslap');
-            expect(msg.content).toContain('xkcd');
-            expect(msg.content).toContain('!meme');
-            expect(msg.content).toContain('!foff');
-            
-            done();
-        });
-    });
+        _lastMessage
+            .pipe(filter(msg => msg.author.username === 'TestSlickBot'))
+            .pipe(filter(msg => msg.content.includes('!help')))
+            .pipe(take(1))
+            .subscribe( msg => {
+                expect(msg.content).toContain('!help');
+                expect(msg.content).toContain('ping');
+                expect(msg.content).toContain('!weather');
+                expect(msg.content).toContain('!troutslap');
+                expect(msg.content).toContain('xkcd');
+                expect(msg.content).toContain('!meme');
+                expect(msg.content).toContain('!foff');
+                done();
+            });
+    }, 10000);
 
     it('foass should f off', (done) => {
         const testChannel = findChannelByName(_userClient.client, TEST_CHANNEL)
         testChannel.send('!foff @everyone');
 
-        _lastMessage.pipe(filter(msg => msg.author.username === 'TestSlickBot')).pipe(take(1)).subscribe( msg => {
-            expect(msg.content).toContain('TestUserBot,');
-            done();
-        });
-    });
+        _lastMessage
+            .pipe(filter(msg => msg.author.username === 'TestSlickBot'))
+            .pipe(filter(msg => msg.content.includes('TestUserBot')))
+            .pipe(take(1))
+            .subscribe( msg => {
+                expect(msg.content).toContain('TestUserBot')
+                done();
+            });
+    }, 10000);
 })
