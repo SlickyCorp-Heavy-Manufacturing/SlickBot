e38a588b1ea93f6e5093b82417cb2754461b4b74
krische <1189328+krische@users.noreply.github.com>
2020-08-19 20:27:52 -0500

    Improve testing

--- src/covid/covid-command.ts
+++ src/covid/covid-command.ts
@@ -1,34 +1,7 @@
 import { ICommand } from '../icommand';
 import { Message, TextChannel } from 'discord.js';
-import got from 'got';
+import { Covid } from './covid';
 
-interface UsDaily {
-    date: string;
-    states: number,
-    positive: number,
-    negative: number,
-    pending: number,
-    hospitalizedCurrently: number,
-    hospitalizedCumulative: number,
-    inIcuCurrently: number,
-    inIcuCumulative: number,
-    onVentilatorCurrently: number,
-    onVentilatorCumulative: number,
-    recovered: number,
-    dateChecked: string,
-    death: number
-    hospitalized: number
-    lastModified: string,
-    total: number
-    totalTestResults: number
-    posNeg: number
-    deathIncrease: number
-    hospitalizedIncrease: number
-    negativeIncrease: number
-    positiveIncrease: number
-    totalTestResultsIncrease: number
-    hash: string
-}
 
 export const CovidCommand: ICommand = {
     name: '!covid',
@@ -36,9 +9,8 @@ export const CovidCommand: ICommand = {
     showInHelp: true,
     trigger: (msg: Message) => msg.content.startsWith('!covid') && (msg.channel as TextChannel).name === 'covid-tendies', 
     command: (msg: Message) => {
-        got('https://covidtracking.com/api/us/daily', { responseType: 'json' }).then((value) => {
-            const body = value.body as UsDaily[];
-            msg.channel.send(`${body[0].deathIncrease} Americans laid down their lives for Mike's tendies today.`);
+        Covid.usDaily().then((usDaily) => {
+            msg.channel.send(usDaily);
         });
     },
 }
