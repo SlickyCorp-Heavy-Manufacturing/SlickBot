7b0b83df075b8c51d28bf58f55b1237848148854
Brian Krische <1189328+krische@users.noreply.github.com>
2020-08-19 22:15:10 -0500

    Refactor tendies class and add a scheduled post. (#31)

---
+++ src/tendies/tendies-types.ts
@@ -0,0 +1,15 @@
+export interface Daily {
+    adjClose: number,
+    adjHigh: number,
+    adjLow: number,
+    adjOpen: number,
+    adjVolume: number,
+    close: number,
+    date: string,
+    divCash: number,
+    high: number,
+    low: number,
+    open: number,
+    splitFactor: number,
+    volume: number
+}
