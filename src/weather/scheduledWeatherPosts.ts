daecb053410ebff0377a0e9c08369bb935717f45
UpsidePotential <48447427+UpsidePotential@users.noreply.github.com>
2020-08-20 08:10:43 -0500

    post a teh right time (#36)

--- src/weather/scheduledWeatherPosts.ts
+++ src/weather/scheduledWeatherPosts.ts
@@ -6,5 +6,5 @@ const WEATHER_CHANNEL = 'noaa-information-bureau';
 export const scheduledWeatherPosts: IScheduledPost[] = [
     { cronDate: '30 8 * * *', channel: WEATHER_CHANNEL, getMessage: () => Weather.currentWeather() },
     { cronDate: '0 12 * * *', channel: WEATHER_CHANNEL, getMessage: () => Weather.currentWeather() },
-    { cronDate: '30 4 * * *', channel: WEATHER_CHANNEL, getMessage: () => Weather.currentWeather() },
+    { cronDate: '30 16 * * *', channel: WEATHER_CHANNEL, getMessage: () => Weather.currentWeather() },
 ];
