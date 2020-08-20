e415c309d8ede6908926b2d47f528d8463edbcfd
Brian Krische <1189328+krische@users.noreply.github.com>
2020-08-20 15:25:57 -0500

    Fix tendies (#38)

--- src/tendies/tendies-schedule.ts
+++ src/tendies/tendies-schedule.ts
@@ -2,5 +2,5 @@ import { IScheduledPost } from "../ischeduledpost";
 import { Tendies } from "./tendies";
 
 export const scheduledTendiesPosts: IScheduledPost[] = [
-    { cronDate: '0 1 15 * * 1-5', channel: 'nsfw', getMessage: () => Tendies.blakesHappiness() },
+    { cronDate: '0 31 16 * * 1-5', channel: 'nsfw', getMessage: () => Tendies.blakesHappiness() }, // Not available until 5:30 PM Eastern
 ];
