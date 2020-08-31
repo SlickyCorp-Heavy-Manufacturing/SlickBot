7230d483c6a7ed8e4bb3b135c489cb7e15fc95b0
UpsidePotential <mikeafuchs@gmail.com>
2020-08-31 14:36:10 -0500

    lint

--- src/haircut/reminder.ts
+++ src/haircut/reminder.ts
@@ -3,5 +3,5 @@ import { IScheduledPost } from '../ischeduledpost';
 const IRL_CHANNEL = 'irl';
 
 export const PulakHairCut: IScheduledPost[] = [
-    { cronDate: '0 9 * * *', channel: IRL_CHANNEL, getMessage: () => Promise.resolve('@pulak haircut day! Dont forget to wish him a happy haircut!') },
+  { cronDate: '0 9 * * *', channel: IRL_CHANNEL, getMessage: () => Promise.resolve('@pulak haircut day! Dont forget to wish him a happy haircut!') },
 ];
