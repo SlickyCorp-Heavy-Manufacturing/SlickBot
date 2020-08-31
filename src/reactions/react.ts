2ae330b61c8f67125f7cb546265d219c05a83034
UpsidePotential <mikeafuchs@gmail.com>
2020-08-31 13:54:08 -0500

    fix lint

--- src/reactions/react.ts
+++ src/reactions/react.ts
@@ -5,7 +5,7 @@ export const NiceReaction: ICommand = {
   name: 'nice reaction',
   helpDescription: '',
   showInHelp: false,
-  trigger: (msg: Message) => new RegExp("\\b69\\b").test(msg.content),
+  trigger: (msg: Message) => new RegExp(`\\b69\\b`).test(msg.content),
   command: async (msg: Message) => {
     await msg.react('🇳');
     await msg.react('🇮');
