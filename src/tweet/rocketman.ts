42d4a60dcd94ebed79e649274a1dd2bf13ef8d80
sjkowal <sjkowal@gmail.com>
2020-09-06 18:17:07 -0500

    let the rocketman tweet

--- src/tweet/rocketman.ts
+++ src/tweet/rocketman.ts
@@ -2,20 +2,20 @@ import { Message } from 'discord.js';
 import { ICommand } from '../icommand';
 import { TweetGen } from './tweet-generator';
 
-const commandString = '!trump';
+const commandString = '!elon';
 
-export const TrompoCommand: ICommand = {
+export const RocketManCommand: ICommand = {
   name: commandString,
-  helpDescription: 'Bot will respond with a turomu tweet',
+  helpDescription: 'Bot will respond with a tweet from the rocket man',
   showInHelp: true,
   trigger: (msg: Message) => msg.content.startsWith(commandString),
   command: async (msg: Message) => {
     const text = msg.cleanContent.substring(commandString.length);
 
     await TweetGen.tweet(msg, {
-      name: 'Donald J. Trump',
-      nickname: 'realDonaldTrump',
-      avatar: 'https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_400x400.jpg',
+      name: 'Elon Musk',
+      nickname: 'elonmusk',
+      avatar: 'https://pbs.twimg.com/profile_images/1295975423654977537/dHw9JcrK_400x400.jpg',
       text,
     });
   },
