19b6374d3cf059360b5bfb366e9dc8837191aa6a
mfredette <mfredette@gmail.com>
2020-09-13 16:08:43 -0500

    yes-bitwise

--- src/play/play.ts
+++ src/play/play.ts
@@ -1,42 +1,41 @@
+/* eslint no-bitwise: ["error", { "allow": ["<<"] }] */
 import * as yargs from 'yargs';
 import { Message } from 'discord.js';
 
 import ytdl from 'ytdl-core-discord';
-import { number } from 'yargs';
 
 export class Play {
   static currentStream: any;
 
   public static async getTrack(msg: Message): Promise<String> {
     if (msg.channel.type === 'dm') Promise.resolve('Don\'t !play with your DMs.');
-    
+
     const args = yargs
-    .number('volume').number('v')
-    .options({
-      url: { type: 'string', default: 'https://youtu.be/dQw4w9WgXcQ' },
-      volume: { type: 'number', alias: 'v', default: '100' }
-    }).parse(msg.content);
+      .number('volume').number('v')
+      .options({
+        url: { type: 'string', default: 'https://youtu.be/dQw4w9WgXcQ' },
+        volume: { type: 'number', alias: 'v', default: '100' },
+      }).parse(msg.content);
 
     // If the url is provided as the last arg (without --url), use that
     if (yargs.argv._.length > 1) {
       args.url = yargs.argv._.pop();
     }
 
-    if (!(args.url.startsWith("https://") &&
-        (args.url.includes("youtube.com") || args.url.includes("youtu.be"))
-        )) {
+    if (!(args.url.startsWith('https://')
+        && (args.url.includes('youtube.com') || args.url.includes('youtu.be'))
+    )) {
       return Promise.resolve(`${args.url} is not a dank enough beat, try again.`);
     }
-    
+
     const voiceChannel = msg.member.voice.channel;
     if (!voiceChannel) {
       return Promise.resolve('Join a voice channel, Dumas!');
     }
-    voiceChannel.members
 
     voiceChannel.join().then(async (connection) => {
-      const stream = await ytdl(args.url, { highWaterMark: 1<<25, filter: 'audioonly' });
-      
+      const stream = await ytdl(args.url, { highWaterMark: 1 << 25, filter: 'audioonly' });
+
       // Kill the current stream, if it exists
       if (Play.currentStream) {
         console.log('Destroying current stream');
@@ -51,8 +50,8 @@ export class Play {
           stream.destroy(err);
         }
       });
-      
-      const dispatcher = connection.play(stream, { type: 'opus' } );
+
+      const dispatcher = connection.play(stream, { type: 'opus' });
       dispatcher.setVolumeLogarithmic(args.v / 100);
       dispatcher.on('error', console.error);
       dispatcher.on('finish', () => voiceChannel.leave());
