f25c4e84feb503656f2388db5f1b5e6a24797932
mfredette <mfredette@gmail.com>
2020-09-13 16:02:49 -0500

    louda

--- src/play/play.ts
+++ src/play/play.ts
@@ -1,32 +1,63 @@
 import * as yargs from 'yargs';
 import { Message } from 'discord.js';
 
-const ytdl = require('ytdl-core-discord');
+import ytdl from 'ytdl-core-discord';
+import { number } from 'yargs';
 
 export class Play {
+  static currentStream: any;
+
   public static async getTrack(msg: Message): Promise<String> {
-    const args = yargs.options({
+    if (msg.channel.type === 'dm') Promise.resolve('Don\'t !play with your DMs.');
+    
+    const args = yargs
+    .number('volume').number('v')
+    .options({
       url: { type: 'string', default: 'https://youtu.be/dQw4w9WgXcQ' },
+      volume: { type: 'number', alias: 'v', default: '100' }
     }).parse(msg.content);
 
-    if (msg.channel.type === 'dm') Promise.resolve('Don\'t !play with your DM');
-
-    const voiceChannel = msg.member.voice.channel;
-
-    if (!voiceChannel) {
-      return Promise.resolve('please join a voice channel first!');
+    // If the url is provided as the last arg (without --url), use that
+    if (yargs.argv._.length > 1) {
+      args.url = yargs.argv._.pop();
     }
 
+    if (!(args.url.startsWith("https://") &&
+        (args.url.includes("youtube.com") || args.url.includes("youtu.be"))
+        )) {
+      return Promise.resolve(`${args.url} is not a dank enough beat, try again.`);
+    }
+    
+    const voiceChannel = msg.member.voice.channel;
+    if (!voiceChannel) {
+      return Promise.resolve('Join a voice channel, Dumas!');
+    }
+    voiceChannel.members
+
     voiceChannel.join().then(async (connection) => {
-      const stream = await ytdl(args.url, { type: 'opus', filter: 'audioonly' });
-      const dispatcher = connection.play(stream);
+      const stream = await ytdl(args.url, { highWaterMark: 1<<25, filter: 'audioonly' });
+      
+      // Kill the current stream, if it exists
+      if (Play.currentStream) {
+        console.log('Destroying current stream');
+        Play.currentStream.destroy();
+      }
+      Play.currentStream = stream;
 
-      // dispatcher.on("debug", console.log);
+      // Kill the stream if the bot is disconnected before the end of the stream
+      connection.on('disconnect', (err) => {
+        if (stream) {
+          console.log('Disconnected, destroying stream');
+          stream.destroy(err);
+        }
+      });
+      
+      const dispatcher = connection.play(stream, { type: 'opus' } );
+      dispatcher.setVolumeLogarithmic(args.v / 100);
       dispatcher.on('error', console.error);
-
       dispatcher.on('finish', () => voiceChannel.leave());
     });
 
-    return Promise.resolve(args.url);
+    return Promise.resolve(`🎶 Slickyboi started playing: ${args.url} 🎶`);
   }
 }
