0a09586d3378134c487e2a0d8203a93961a12963
krische <1189328+krische@users.noreply.github.com>
2020-10-15 09:04:53 -0500

    Add test

--- src/devops/devops.ts
+++ src/devops/devops.ts
@@ -1,4 +1,4 @@
-import { Message } from 'discord.js';
+import { Message, User } from 'discord.js';
 import { sample as _sample } from 'lodash';
 import { ICommand } from '../icommand';
 
@@ -8,15 +8,37 @@ export const DevOpsCommand: ICommand = {
   showInHelp: true,
   trigger: (msg: Message) => msg.cleanContent.toLocaleLowerCase().startsWith('@devops'),
   command: async (msg: Message) => {
-    // Create list of DevOps users
-    const devopsUsers = [
-      msg.client.users.cache.find((user) => user.username === 'krische'),
-      msg.client.users.cache.find((user) => user.username === 'freedeau'),
-    ].filter((user) => user !== undefined);
-
-    const id = `DevOps01-${Math.floor(Math.random() * 9000) + 1000}`;
-    const description = `As a DevOps customer, ${msg.cleanContent.replace(/^\@devops\s*/i, '')}`;
-    const points = _sample([2, 3, 5, 8, 13]);
-    await msg.channel.send(`<@${_sample(devopsUsers).id}> ${id} (${points} points) has been created and assigned to you.\n> ${description}`);
+    const story = DevOpsStory.fromMessage(msg);
+    await msg.channel.send(`<@${story.assignee.id}> ${story.id} (${story.points} points) has been created and assigned to you.\n> ${story.description}`);
   },
 };
+
+export class DevOpsStory {
+  private static readonly DEVOPS_USERS = ['krische', 'freedeau'];
+
+  public readonly assignee: User;
+  public readonly description: string;
+  public readonly id: string;
+  public readonly points: number;
+
+  constructor(assignee: User, description: string, id: string, points: number) {
+    this.assignee = assignee;
+    this.description = description;
+    this.id = id;
+    this.points = points;
+  }
+
+  /**
+   * Create a new DevOps story from a discord message
+   * @param msg The Discord message
+   */
+  public static fromMessage(msg: Message): DevOpsStory {
+    const potentialAssignees = this.DEVOPS_USERS.map((username: string) => msg.client.users.cache.find((user) => user.username === username)).filter((user) => user !== undefined);
+
+    const id = `DevOps01-${Math.floor(Math.random() * 9000) + 1000}`;
+    const description = `As a DevOps customer, ${msg.cleanContent.replace(/^@devops\s*/i, '')}`;
+    const points = _sample([2, 3, 5, 8, 13]);
+
+    return new DevOpsStory(_sample(potentialAssignees), description, id, points);
+  }
+}
