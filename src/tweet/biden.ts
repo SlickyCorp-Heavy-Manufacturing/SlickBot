a5175338e366ff7b12b37aaf5531281af606a052
krische <1189328+krische@users.noreply.github.com>
2020-11-04 21:42:01 -0600

    More tweets

---
+++ src/tweet/biden.ts
@@ -0,0 +1,25 @@
+import { Message } from 'discord.js';
+import { ICommand } from '../icommand';
+import { TweetGen } from './tweet-generator';
+
+const commandString = '!biden';
+
+export const RocketManCommand: ICommand = {
+  name: commandString,
+  helpDescription: 'Bot will respond with a tweet from Joe Biden',
+  showInHelp: true,
+  trigger: (msg: Message) => msg.content.startsWith(commandString),
+  command: async (msg: Message) => {
+    const text = msg.cleanContent.substring(commandString.length);
+
+    await TweetGen.tweet(msg, {
+      name: 'Joe Biden',
+      nickname: 'JoeBiden',
+      avatar: 'https://pbs.twimg.com/profile_images/1308769664240160770/AfgzWVE7_400x400.jpg',
+      text,
+      retweets: Math.floor(Math.random() * (654321 - 123456 + 1) + 123456),
+      retweetsWithComments: Math.floor(Math.random() * (54321 - 12345 + 1) + 12345),
+      likes: Math.floor(Math.random() * (654321 - 123456 + 1) + 123456),
+    });
+  },
+};
