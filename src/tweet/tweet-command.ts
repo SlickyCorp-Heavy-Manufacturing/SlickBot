803e510e30512fac8bfadf8bb824d636bedd0b40
Brian Krische <1189328+krische@users.noreply.github.com>
2020-11-05 21:54:37 -0600

    Fix !tweet command

--- src/tweet/tweet-command.ts
+++ src/tweet/tweet-command.ts
@@ -3,19 +3,19 @@ import { Message } from 'discord.js';
 import { ICommand } from '../icommand';
 import { TweetGen } from './tweet-generator';
 
-const messageRegex = RegExp(/^!tweet\s+(?<handle>\w+)\s+(?<text>.+)/gms);
+const messageRegex = /^!tweet\s+(\w+)\s+(.+)/gims;
 
 export const TweetCommand: ICommand = {
   name: '!tweet',
   helpDescription: 'Bot will respond with a tweet from given twitter user',
   showInHelp: true,
-  trigger: (msg: Message) => messageRegex.test(msg.content),
+  trigger: (msg: Message) => msg.content.startsWith('!tweet'),
   command: async (msg: Message) => {
-    const match = msg.cleanContent.match(messageRegex);
+    const match = messageRegex.exec(msg.content);
 
     await TweetGen.tweet(msg, {
-      nickname: match.groups.handle,
-      text: match.groups.text,
+      nickname: match[1],
+      text: match[2],
       retweets: Math.floor(Math.random() * (12345 - 0 + 1) + 0),
       retweetsWithComments: Math.floor(Math.random() * (1234 - 0 + 1) + 0),
       likes: Math.floor(Math.random() * (12345 - 0 + 1) + 0),
