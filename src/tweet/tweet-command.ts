d4d8cdc91905d055aeded2688c0ca403d3f4dcf5
krische <1189328+krische@users.noreply.github.com>
2020-11-05 21:24:28 -0600

    ffs

--- src/tweet/tweet-command.ts
+++ src/tweet/tweet-command.ts
@@ -11,13 +11,13 @@ export const TweetCommand: ICommand = {
   showInHelp: true,
   trigger: (msg: Message) => msg.content.startsWith('!tweet'),
   command: async (msg: Message) => {
-    msg.channel.send('```javascript' + "\n" + JSON.stringify(messageRegex.exec(msg.content)) + '```');
-    const { groups: { handle, text }} = messageRegex.exec(msg.content);
-    msg.channel.send('```javascript' + "\n" + JSON.stringify({handle, text}) + '```');
+    msg.channel.send(`\`\`\`javascript\n${JSON.stringify(messageRegex.exec(msg.content))}\`\`\``);
+    const { groups: { handle, text } } = messageRegex.exec(msg.content);
+    msg.channel.send(`\`\`\`javascript\n${JSON.stringify({ handle, text })}\`\`\``);
 
     await TweetGen.tweet(msg, {
       nickname: handle,
-      text: text,
+      text,
       retweets: Math.floor(Math.random() * (12345 - 0 + 1) + 0),
       retweetsWithComments: Math.floor(Math.random() * (1234 - 0 + 1) + 0),
       likes: Math.floor(Math.random() * (12345 - 0 + 1) + 0),
