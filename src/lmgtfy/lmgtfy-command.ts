4c53e94c6edb76b38402c0a57d03cf0788bff131
UpsidePotential <mikeafuchs@gmail.com>
2020-12-16 09:39:17 -0600

    fix lint

--- src/lmgtfy/lmgtfy-command.ts
+++ src/lmgtfy/lmgtfy-command.ts
@@ -7,9 +7,9 @@ export const LMGTFYCommand: ICommand = {
   showInHelp: true,
   trigger: (msg: Message) => msg.cleanContent.startsWith('!lmgtfy'),
   command: async (msg: Message) => {
-    msg.channel.messages.fetch({limit: 2}).then( messages => {
-        const message = messages.last().cleanContent.replace(/\s/g, '+')
-        msg.reply(`https://lmgtfy.app/?q=${message}`)
-    })
+    msg.channel.messages.fetch({ limit: 2 }).then((messages) => {
+      const message = messages.last().cleanContent.replace(/\s/g, '+');
+      msg.reply(`https://lmgtfy.app/?q=${message}`);
+    });
   },
 };
