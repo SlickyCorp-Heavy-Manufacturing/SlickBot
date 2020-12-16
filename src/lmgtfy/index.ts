450eb1f304d1587658e16d1f32518fb901a14e71
UpsidePotential <48447427+UpsidePotential@users.noreply.github.com>
2020-12-16 09:44:14 -0600

    lmgtfy (#104)
    * ship it
    
    * fix lint

---
+++ src/lmgtfy/index.ts
@@ -0,0 +1,1 @@
+export { LMGTFYCommand } from './lmgtfy-command';
---
+++ src/lmgtfy/lmgtfy-command.ts
@@ -0,0 +1,15 @@
+import { Message } from 'discord.js';
+import { ICommand } from '../icommand';
+
+export const LMGTFYCommand: ICommand = {
+  name: 'lmgtfy',
+  helpDescription: 'There are no stupid questions...',
+  showInHelp: true,
+  trigger: (msg: Message) => msg.cleanContent.startsWith('!lmgtfy'),
+  command: async (msg: Message) => {
+    msg.channel.messages.fetch({ limit: 2 }).then((messages) => {
+      const message = messages.last().cleanContent.replace(/\s/g, '+');
+      msg.reply(`https://lmgtfy.app/?q=${message}`);
+    });
+  },
+};
