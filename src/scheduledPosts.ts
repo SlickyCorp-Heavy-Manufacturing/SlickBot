31d48549b58feb23142b2c1983cbe4d9af65f0f9
krische <1189328+krische@users.noreply.github.com>
2021-03-10 09:11:23 -0600

    Suggest hangover cures

--- src/scheduledPosts.ts
+++ src/scheduledPosts.ts
@@ -3,8 +3,10 @@ import { scheduledTendiesPosts } from './tendies/tendies-schedule';
 import { scheduledTweetChecks } from './tweet/tweet-scheduler';
 import { scheduledWeatherPosts } from './weather';
 import { PulakHairCut } from './haircut/reminder';
+import { scheduledHangoverCures } from './hungover';
 
 export const scheduledPosts: IScheduledPost[] = [
+  ...scheduledHangoverCures,
   ...scheduledTendiesPosts,
   ...scheduledTweetChecks,
   ...scheduledWeatherPosts,
