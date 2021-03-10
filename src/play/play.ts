c62cb16ecd9aefc4e3cbec36165be75b455e0c2f
Brian Krische <1189328+krische@users.noreply.github.com>
2021-03-10 14:43:57 -0600

    Support text to speech (#141)

--- src/play/play.ts
+++ src/play/play.ts
@@ -1,16 +1,25 @@
 /* eslint no-bitwise: ["error", { "allow": ["<<"] }] */
+import * as googleTTS from 'google-tts-api';
 import * as yargs from 'yargs';
 import { Message, VoiceChannel } from 'discord.js';
 
 import Soundcloud from 'soundcloud.ts';
 import ytdl from 'discord-ytdl-core';
 
+interface PlayItem {
+  msg: Message;
+  text?: string;
+  url: string;
+  voiceChannel: VoiceChannel;
+  volume: number;
+}
+
 export class Play {
   private static currentlyPlaying = false;
 
   private static currentStream: any;
 
-  private static queue: {(): void;}[] = [];
+  private static queue: { (): void; }[] = [];
 
   public static async playTrack(msg: Message, playNow: boolean): Promise<void> {
     if (msg.channel.type === 'dm') {
@@ -19,27 +28,8 @@ export class Play {
     }
 
     const DRUNK_ROLE = msg.guild.roles.cache.find((role) => role.name === 'drunk');
-    if (msg.member.roles.cache.has(DRUNK_ROLE.id)) {
+    if (DRUNK_ROLE && msg.member.roles.cache.has(DRUNK_ROLE.id)) {
       await msg.reply('You\'re drunk, go home.');
-      return;
-    }
-
-    const args = yargs
-      .number('volume').number('v')
-      .options({
-        url: { type: 'string', default: 'https://youtu.be/dQw4w9WgXcQ' },
-        volume: { type: 'number', alias: 'v', default: '100' },
-      }).parse(msg.content);
-
-    // If the url is provided as the last arg (without --url), use that
-    if (yargs.argv._.length > 1) {
-      args.url = yargs.argv._.pop();
-    }
-
-    if (!(args.url.startsWith('https://')
-        && (args.url.includes('youtube.com') || args.url.includes('youtu.be') || args.url.includes('soundcloud.com'))
-    )) {
-      await msg.reply(`${args.url} is not a dank enough beat, try again.`);
       return;
     }
 
@@ -49,10 +39,50 @@ export class Play {
       return;
     }
 
-    if (playNow) {
-      this.startJamming(msg, voiceChannel, args.url, args.v);
+    // Does this contain a URL?
+    let item: PlayItem;
+    if (msg.content.includes('http')) {
+      const args = yargs
+        .number('volume').number('v')
+        .options({
+          url: { type: 'string', default: 'https://youtu.be/dQw4w9WgXcQ' },
+          volume: { type: 'number', alias: 'v', default: '100' },
+        }).parse(msg.content);
+
+      // If the url is provided as the last arg (without --url), use that
+      if (yargs.argv._.length > 1) {
+        args.url = yargs.argv._.pop();
+      }
+
+      if (!(args.url.startsWith('https://')
+        && (args.url.includes('youtube.com') || args.url.includes('youtu.be') || args.url.includes('soundcloud.com'))
+      )) {
+        await msg.reply(`${args.url} is not a dank enough beat, try again.`);
+        return;
+      }
+
+      // Save this item to be played or added to queue
+      item = {
+        msg,
+        url: args.url,
+        voiceChannel,
+        volume: args.v,
+      };
     } else {
-      const queuedTrackCallback = () => this.startJamming(msg, voiceChannel, args.url, args.v);
+      const text = msg.content.replace(/^![^\s]+\s+/, '');
+      item = {
+        msg,
+        text,
+        url: googleTTS.getAudioUrl(text),
+        voiceChannel,
+        volume: 100,
+      };
+    }
+
+    if (playNow) {
+      this.startJamming(item);
+    } else {
+      const queuedTrackCallback = () => this.startJamming(item);
       this.queue.push(queuedTrackCallback);
       this.processQueue();
     }
@@ -72,64 +102,67 @@ export class Play {
    * @param url The track to play
    * @param volume The volume to play the track at
    */
-  private static async startJamming(
-    msg: Message,
-    voiceChannel: VoiceChannel,
-    url: string,
-    volume: number,
-  ): Promise<void> {
+  private static async startJamming(item: PlayItem): Promise<void> {
     this.currentlyPlaying = true;
     let stream: any;
     const clientId = 'ymSs5c4hxrRu3yuA6ZyOORoADJI2tVPD';
     const soundcloud = new Soundcloud(clientId);
 
     try {
-      if (url.includes('soundcloud.com')) {
-        stream = await soundcloud.util.streamTrack(url);
+      if (item.url.includes('soundcloud.com')) {
+        stream = await soundcloud.util.streamTrack(item.url);
+      } else if (item.url.includes('translate.google.com')) {
+        stream = item.url;
       } else {
-        stream = ytdl(url, {
+        stream = ytdl(item.url, {
           filter: 'audioonly',
           highWaterMark: 1 << 25,
           opusEncoded: true,
         });
       }
     } catch (error) {
-      await msg.reply(`Slickyboi pooped: ${error} 🎶`);
+      await item.msg.reply(`Slickyboi pooped: ${error} 🎶`);
       return;
     }
 
-    voiceChannel.join().then(async (connection) => {
+    item.voiceChannel.join().then(async (connection) => {
       // Kill the current stream, if it exists
       if (Play.currentStream) {
         console.log('Destroying current stream');
-        Play.currentStream.destroy();
+        if (Play.currentStream.destroy) {
+          Play.currentStream.destroy();
+        }
         Play.currentStream = undefined;
       }
       Play.currentStream = stream;
 
       // Kill the stream if the bot is disconnected before the end of the stream
       connection.on('disconnect', (err) => {
-        if (stream) {
+        if (stream && stream.destroy) {
           console.log('Disconnected, destroying stream');
           stream.destroy(err);
         }
       });
 
       let options = {};
-      if (!url.includes('soundcloud.com')) {
+      if (!item.url.includes('soundcloud.com') && !item.url.includes('translate.google.com')) {
         options = { type: 'opus' };
       }
 
       const dispatcher = connection.play(stream, options);
-      dispatcher.setVolumeLogarithmic(volume / 100);
+      dispatcher.setVolumeLogarithmic(item.volume / 100);
       dispatcher.on('error', console.error);
       dispatcher.on('finish', () => {
-        voiceChannel.leave();
+        item.voiceChannel.leave();
         this.currentlyPlaying = false;
         setTimeout(() => this.processQueue(), 100);
       });
 
-      await msg.reply(`🎶 Slickyboi started playing: ${url} 🎶`);
+      if (item.url.includes('translate.google.com')) {
+        await item.msg.reply(`🎙️ Slickyboi started speaking: ${item.text} 🎙️`);
+      } else {
+        await item.msg.reply(`🎶 Slickyboi started playing: ${item.url} 🎶`);
+      }
     });
   }
 }
