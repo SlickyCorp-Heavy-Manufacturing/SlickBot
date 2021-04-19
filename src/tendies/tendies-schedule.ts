e198d31773daa9f78068d323190569f3271c5179
krische <1189328+krische@users.noreply.github.com>
2021-04-19 12:15:31 -0500

    some code changes

--- src/tendies/tendies-schedule.ts
+++ src/tendies/tendies-schedule.ts
@@ -1,6 +1,4 @@
 import { IScheduledPost } from '../ischeduledpost';
 import { Tendies } from './tendies';
 
-export const scheduledTendiesPosts: IScheduledPost[] = [
-  { cronDate: '0 1 15 * * 1-5', channel: '619704904696594493', getMessage: () => Tendies.blakesHappiness() }, // Markets typically close at 4:00 PM Eastern
-];
+export const scheduledTendiesPosts: IScheduledPost[] = [];
