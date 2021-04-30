c4445d3fb8c0b5e3c130e20e36bd82b8e49a4153
Mark Tutkowski <tutkowskim@users.noreply.github.com>
2021-04-30 14:12:51 -0500

    Add average hash rate (#164)
    * Add the miner stats command
    
    * Fix docs
    
    * Add average hashrate
    
    * fix lint errors
    
    Co-authored-by: Mark Tutkowski <mark@tutkowski.com>

--- src/miner/miner-stats-command.ts
+++ src/miner/miner-stats-command.ts
@@ -4,6 +4,8 @@ import { EthermineDashboard } from './ethermine';
 
 const walletRegex = new RegExp('0x[A-Za-z0-9]+');
 
+const average = (array: number[]) => array.reduce((a, b) => a + b) / array.length;
+
 export const MinerStatsCommand: ICommand = {
   name: '!miner-stats',
   helpDescription: 'Get the statistics of a miner on ethermine. Usage "!miner-stats <wallet-address>"',
@@ -17,9 +19,12 @@ export const MinerStatsCommand: ICommand = {
     }
 
     const info = await EthermineDashboard.info(walletAddress);
+    const averageHashRate = average(info.data.statistics.map((stats) => stats.currentHashrate));
+
     await msg.reply(`Ethermine statistics for ${walletAddress}:
 > Active Workers: ${info.data.currentStatistics.activeWorkers}
 > Current Hashrate: ${(info.data.currentStatistics.currentHashrate / 1000000).toFixed(3)} MH/s
+> Average Hashrate: ${(averageHashRate / 1000000).toFixed(3)} MH/s
 > Invalid Shares: ${info.data.currentStatistics.invalidShares}
 > Stale Shares: ${info.data.currentStatistics.staleShares}
 > Valid Shares: ${info.data.currentStatistics.validShares}`);
