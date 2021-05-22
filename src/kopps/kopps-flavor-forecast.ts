95eb75745942e13a8f282f88d8d9ad3244613168
Mark Tutkowski <mark@tutkowski.com>
2021-05-22 13:41:05 -0500

    cleanup lint errors

--- src/kopps/kopps-flavor-forecast.ts
+++ src/kopps/kopps-flavor-forecast.ts
@@ -16,7 +16,10 @@ export class KoppsFlavorForecast {
   public static async flavorForecast(): Promise<FlavorForecast[]> {
     const flavorPreviewHtml = (await got('https://www.kopps.com/flavor-preview')).body;
     const flavorCardsHtmlElements = KoppsFlavorForecast.getFlavorCardsHTML(flavorPreviewHtml);
-    return flavorCardsHtmlElements.map((flavorCardsHtmlElement: string) => KoppsFlavorForecast.getFlavorForecastFromHTML(flavorCardsHtmlElement));
+    return flavorCardsHtmlElements.map((flavorCardsHtmlElement: string) => {
+      const flavors = KoppsFlavorForecast.getFlavorForecastFromHTML(flavorCardsHtmlElement);
+      return flavors;
+    });
   }
 
   public static async flavorOfTheDay(): Promise<FlavorForecast> {
@@ -29,22 +32,22 @@ export class KoppsFlavorForecast {
 
   public static getFlavorCardsHTML(flavorPreviewHtml: string): string[] {
     const $ = cheerio.load(flavorPreviewHtml);
-    return $('.flavor-forecast').toArray().map(forecastCard => $.html(forecastCard));
+    return $('.flavor-forecast').toArray().map((forecastCard) => $.html(forecastCard));
   }
-  
+
   private static getFlavorForecastFromHTML(flavorCardHtml: string): FlavorForecast {
     const $ = cheerio.load(flavorCardHtml);
-  
+
     const flavors: Flavor[] = [];
-    for (let i = 1; i<= 2; ++i) {
+    for (let i = 1; i <= 2; i += 1) {
       const flavor = $(`.grid div:nth-of-type(${i}) .flavor-of-day`).text()?.trim();
       const description = $(`.grid div:nth-of-type(${i}) p`).text()?.trim();
-      const image = 'https://www.kopps.com' + $(`.grid div:nth-of-type(${i}) .flavor-circle img`).prop('src');
-      if (flavor && description && image) { 
+      const image = `https://www.kopps.com${$(`.grid div:nth-of-type(${i}) .flavor-circle img`).prop('src')}`;
+      if (flavor && description && image) {
         flavors.push({ flavor, description, image });
       }
     }
-  
+
     return { date: $('h5').text(), flavors };
   }
 }
