86d0020357b35c9f925266316229c8063ae98117
Mark Tutkowski <tutkowskim@users.noreply.github.com>
2021-05-22 13:44:12 -0500

    Move the kopps flavor forecast webscrapping into this service, since … (#167)
    * Move the kopps flavor forecast webscrapping into this service, since the previous service is being shutdown.
    
    * cleanup lint errors
    
    Co-authored-by: Mark Tutkowski <mark@tutkowski.com>

--- src/kopps/kopps-flavor-forecast.ts
+++ src/kopps/kopps-flavor-forecast.ts
@@ -1,4 +1,5 @@
 import got from 'got';
+import cheerio from 'cheerio';
 
 export interface Flavor {
   flavor: string;
@@ -12,12 +13,13 @@ export interface FlavorForecast {
 }
 
 export class KoppsFlavorForecast {
-  // API that scraps kopps.com to determine the flavor forecast
-  private static readonly KOPPS_FORECAST: string = 'https://kopps-flavor-forecast.tutkowski.com/';
-
   public static async flavorForecast(): Promise<FlavorForecast[]> {
-    const response = await got(KoppsFlavorForecast.KOPPS_FORECAST, { responseType: 'json' });
-    return response.body as FlavorForecast[];
+    const flavorPreviewHtml = (await got('https://www.kopps.com/flavor-preview')).body;
+    const flavorCardsHtmlElements = KoppsFlavorForecast.getFlavorCardsHTML(flavorPreviewHtml);
+    return flavorCardsHtmlElements.map((flavorCardsHtmlElement: string) => {
+      const flavors = KoppsFlavorForecast.getFlavorForecastFromHTML(flavorCardsHtmlElement);
+      return flavors;
+    });
   }
 
   public static async flavorOfTheDay(): Promise<FlavorForecast> {
@@ -26,5 +28,26 @@ export class KoppsFlavorForecast {
       return flavorForecast[0];
     }
     return Promise.reject(new Error('Flavor of the day not found'));
+  }
+
+  public static getFlavorCardsHTML(flavorPreviewHtml: string): string[] {
+    const $ = cheerio.load(flavorPreviewHtml);
+    return $('.flavor-forecast').toArray().map((forecastCard) => $.html(forecastCard));
+  }
+
+  private static getFlavorForecastFromHTML(flavorCardHtml: string): FlavorForecast {
+    const $ = cheerio.load(flavorCardHtml);
+
+    const flavors: Flavor[] = [];
+    for (let i = 1; i <= 2; i += 1) {
+      const flavor = $(`.grid div:nth-of-type(${i}) .flavor-of-day`).text()?.trim();
+      const description = $(`.grid div:nth-of-type(${i}) p`).text()?.trim();
+      const image = `https://www.kopps.com${$(`.grid div:nth-of-type(${i}) .flavor-circle img`).prop('src')}`;
+      if (flavor && description && image) {
+        flavors.push({ flavor, description, image });
+      }
+    }
+
+    return { date: $('h5').text(), flavors };
   }
 }
