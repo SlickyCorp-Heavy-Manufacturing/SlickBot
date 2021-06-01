e413b7e154a340894c3e0cff44338fb59957352f
Brian Krische <1189328+krische@users.noreply.github.com>
2021-06-01 15:03:20 -0500

    Group reaction (#169)

--- src/reactions/react.ts
+++ src/reactions/react.ts
@@ -23,3 +23,21 @@ export const ChulasRecation: ICommand = {
     await msg.react('🤦‍♂️');
   },
 };
+
+export const GroupReaction: ICommand = {
+  name: 'Group reaction',
+  helpDescription: '',
+  showInHelp: false,
+  trigger: (msg: Message) => msg.channel.id === '619704904696594493',
+  command: async (msg: Message) => {
+    const groupA = await msg.guild.roles.fetch('849368883689291776', false, true);
+    if (groupA.members.some((member) => member.user.id === msg.author.id)) {
+      await msg.react('A');
+    } else {
+      const groupB = await msg.guild.roles.fetch('849368974083883038', false, true);
+      if (groupB.members.some((member) => member.user.id === msg.author.id)) {
+        await msg.react('B');
+      }
+    }
+  },
+};
