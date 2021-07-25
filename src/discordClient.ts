7308d3dda3653f6d54e561fb9e941ae4f6e1eacd
Mark Tutkowski <mark@tutkowski.com>
2021-07-25 13:56:25 -0500

    Fix promise

--- src/discordClient.ts
+++ src/discordClient.ts
@@ -24,7 +24,7 @@ export class DiscordClient {
 
             client.user.setActivity(DiscordClient.note());
 
-            resolve();
+            resolve(client);
           });
         });
       }
