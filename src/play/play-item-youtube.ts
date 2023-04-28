9a286932a045f97f8051485a0502f990c0a4a1ff
Brian Krische <1189328+krische@users.noreply.github.com>
2023-04-28 10:33:45 -0500

    Try using ytdl-core directly

--- src/play/play-item-youtube.ts
+++ src/play/play-item-youtube.ts
@@ -1,7 +1,6 @@
 import { AudioResource, createAudioResource, demuxProbe } from '@discordjs/voice';
 import { Message } from 'discord.js';
-import { exec as ytdl } from 'youtube-dl-exec';
-import { getInfo } from 'ytdl-core';
+import ytdl = require('ytdl-core');
 import { PlayItem } from './play-item';
 
 export class PlayItemYoutube implements PlayItem {
@@ -35,46 +34,15 @@ export class PlayItemYoutube implements PlayItem {
     this.volume = volume;
   }
 
-  public createAudioResource(): Promise<AudioResource<PlayItem>> {
-    return new Promise((resolve, reject) => {
-      const process = ytdl(
-        this.url,
-        {
-          format: 'bestaudio',
-          limitRate: '800K',
-          output: '-',
-          quiet: true,
-        },
-        {
-          stdio: ['ignore', 'pipe', 'ignore'],
-        },
-      );
-      if (!process.stdout) {
-        reject(new Error('No stdout'));
-        return;
-      }
-      const stream = process.stdout;
-      const onError = (err: any) => {
-        if (!process.killed) {
-          process.kill();
-        }
-        stream.resume();
-        reject(err);
-      };
-      process
-        .once('spawn', () => {
-          demuxProbe(stream)
-            .then((probe: { stream: any; type: any; }) => resolve(
-              createAudioResource(probe.stream, { metadata: this, inputType: probe.type }),
-            ))
-            .catch(onError);
-        })
-        .catch(onError);
-    });
+  public async createAudioResource(): Promise<AudioResource<PlayItem>> {
+    return createAudioResource(
+      ytdl(this.url, { filter: 'audioonly', dlChunkSize: 0 }),
+      { metadata: this },
+    );
   }
 
   public static async from(msg: Message, url: string, volume: number) {
-    const info = await getInfo(url);
+    const info = await ytdl.getInfo(url);
     return new PlayItemYoutube(msg, info.videoDetails.title, url, volume);
   }
 }
