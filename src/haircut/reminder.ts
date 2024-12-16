880ca4e59e9ebbddf8061b32bc7cad8ad98b010e
Brian Krische <1189328+krische@users.noreply.github.com>
2024-12-16 19:06:09 +0000

    fix(scheduled): Use different library for scheduled posts

--- src/haircut/reminder.ts
+++ src/haircut/reminder.ts
@@ -1,3 +1,5 @@
+import { Client } from 'discord.js';
+
 import { IScheduledPost } from '../ischeduledpost.js';
 
 const IRL_CHANNEL = '614482862363770903';
@@ -6,7 +8,7 @@ export const PulakHairCut: IScheduledPost[] = [
   {
     cronDate: '0 9 * * 4',
     channel: IRL_CHANNEL,
-    getMessage: (client) => {
+    getMessage: (client: Client) => {
       const pulakUser = client.users.cache.find((user) => user.username === 'Pulak');
       return Promise.resolve(`<@${pulakUser.id}> reminder, its haircut day! Don't forget it's tax deductible`);
     },
