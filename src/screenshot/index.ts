bb06e856f9bca985cdc9da2db2d3abb56ece2916
Brian Krische <1189328+krische@users.noreply.github.com>
2024-12-21 09:13:07 -0600

    fix(screenshot): launch browser with --no-sandbox (#749)

--- src/screenshot/index.ts
+++ src/screenshot/index.ts
@@ -72,6 +72,7 @@ export default class Screenshot {
       console.log('Loading browser for screenshot...');
       // Load Browser
       browser = await puppeteer.launch({
+        args: ['--no-sandbox'],
         browser: 'chrome',
         defaultViewport: null,
         headless: true,
